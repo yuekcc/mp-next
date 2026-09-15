c0d4da667d4d5970b72538f766e82006b1b2c5ae
yuekcc <yuekcc@qq.com>
2026-09-16 00:28:05 +0800

    增加测试脚本

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SYSTEM_PROMPT="You're useful AI assistant"
+
+./build/llmcli --api-url http://127.0.0.1:8001/v1/chat/completions \
+    --model any \
+    --api-key sk-1234 \
+    --system-prompt "${SYSTEM_PROMPT}" \
+    --debug \
+    "$@"
